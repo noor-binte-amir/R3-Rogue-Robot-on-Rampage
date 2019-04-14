277eec380445b1a594b72796b78bd3229dc1d86d
Noor Binte Amir <noor.binte.amir@gmail.com>
2019-04-14 18:56:10 +0500

    Add Buildings

---
+++ Assets/Buildings/Scripts/Destroy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+    public GameObject robot;
+    EnemyMovement em;
+    public GameObject destroyedVersion;
+    private Vector3 fixedPos;
+    private Quaternion fixedRotation;
+    public bool HoldsHeart=false;
+
+    private void Start()
+    {
+        em = robot.GetComponent<EnemyMovement>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("entered");
+        if (other.tag == "hand")
+        {
+            //fixedPos = transform.position;
+            //fixedPos.y = transform.position.y - 5f;
+
+            //fixedRotation = transform.rotation;
+
+            //fixedRotation.y = 90;
+            //fixedRotation.y = 180;
+
+            Debug.Log("hand");
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Debug.Log("fall");
+            gameObject.SetActive(false);
+            
+        }
+        
+    }
+    
+}
---
+++ Assets/Buildings/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Destroy(gameObject, 30);
+        Debug.Log("destroyed");
+
+    }
+}
